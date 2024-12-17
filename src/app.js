7896bd353f90f3225e3f8387c9fd0378c9424a1d
pratim2003 <duttapratim003@gmail.com>
2024-12-17 22:12:03 +0530

    working on utils

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({extended : true}))
 
 const whiteList = []
 
