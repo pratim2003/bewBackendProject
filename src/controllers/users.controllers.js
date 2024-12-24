7fd845259bcbda468a7387cf1661d4eb15a2845f
pratim2003 <duttapratim003@gmail.com>
2024-12-24 22:40:05 +0530

    changes

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,9 +2,17 @@ import { userModel } from "../models/user.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const handleGetData = asyncHandler(async(req,res)=>{
+
+    const body = req.body
+
+
     res.status(200).json({
         mssg : "hello Pratim"
     })
 })
 
-export {handleGetData}
+const hadleUploadData= asyncHandler(async(req,res)=>{
+
+})
+
+export {handleGetData,hadleUploadData}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import {handleGetData} from "../controllers/users.controllers.js"
+import {handleGetData,hadleUploadData} from "../controllers/users.controllers.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router  = Router()
 
-router.get("/get",handleGetData)
+router.get("/get",handleGetData).post("/post",upload.fields([
+    {
+        name : "avatar",
+        maxCount : 1
+    },
+    {
+        name : "coverImage",
+        maxCount : 1
+    }
+]),hadleUploadData)
 
 
 export default router
