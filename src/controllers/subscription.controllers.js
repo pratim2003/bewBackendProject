5ca04789bc1a36536e553e7f9b7795eea5a73f3b
pratim2003 <duttapratim003@gmail.com>
2025-02-10 22:54:51 +0530

    Add unsubscribe and isSubscribed functionality to subscription controller and routes

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import {subscriptionModel} from "../models/subscription.model.js"
 import asyncHandler from "../utils/asyncHandler.js"
 
@@ -6,14 +7,57 @@ const subscribe = asyncHandler(async(req,res)=>{
     if(!req.user) return res.status(404).json({message : "no user found"})
     if(!id) return res.status(404).json({message : "no id found"})
     if(req.user._id==id) return res.status(400).json({message : "you cant subscribe yourself"})
+    const subDetail = await subscriptionModel.aggregate([
+        {
+            $match : {
+                Subscriber : new mongoose.Types.ObjectId(req.user._id),
+                channel : new mongoose.Types.ObjectId(id)
+            }
+        }
+    ])
+    if(subDetail.length>0) return res.status(400).json({message : "already subscribed"})
     const data = await subscriptionModel.create({
-        Subscriber : id,
-        channel : req.user._id
+        Subscriber : req.user._id,
+        channel : id
     })
     if(!data) return res.status(500).json({message : "internal servar problem"})
+    console.log(subDetail)
     return res.status(200).json({message : "subscribed"})
+})
+const unsubscribe = asyncHandler(async(req,res)=>{
+    const channelId = req.params.id
+    const subscriberId = req.user._id
+    const id = await subscriptionModel.aggregate([
+        {
+            $match : {
+                Subscriber : new mongoose.Types.ObjectId(subscriberId),
+                channel : new mongoose.Types.ObjectId(channelId),
+            }
+        }
+    ])
+    if(!(id.length>0)) return res.status(400).json({message : "you are not not subscriber of this channel"})
+    await subscriptionModel.findByIdAndDelete(id[0]._id)
+    return res.status(200).json({message : "unsubscribed successful"})
+})
+
+const isSubscribed = asyncHandler(async(req,res)=>{
+    const subscriberId = req.user._id
+    const channelId = req.params.id
+    const check = await subscriptionModel.aggregate([
+        {
+            $match : {
+                Subscriber : new mongoose.Types.ObjectId(subscriberId),
+                channel : new mongoose.Types.ObjectId(channelId),
+            }
+        }
+    ])
+    console.log(check)
+    if(check.length>0) return res.status(200).json({check : true,message : "subscribed"})
+    return res.status(200).json({check : false,message : "unsubcribed"})
 })
 
 export{
-    subscribe
+    subscribe,
+    unsubscribe,
+    isSubscribed
 }
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,9 +1,10 @@
-import {subscribe} from "../controllers/subscription.controllers.js"
+import {subscribe,unsubscribe,isSubscribed} from "../controllers/subscription.controllers.js"
 import { Router } from "express"
 import { auth } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
 router.post("/subscribe/:id",auth,subscribe)
-
+router.delete("/unsubscribe/:id",auth,unsubscribe)
+router.post("/issubscribe/:id",auth,isSubscribed)
 export default router
